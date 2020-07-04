04a37b2fa902057fc61308f262630304d5c34783
Renato Rocha <renatob.rocha@gmail.com>
2020-07-04 01:10:30 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
-export default function App() {
+import React, { useRef } from 'react';
+
+import { StyleSheet } from 'react-native';
+import { State, PinchGestureHandler, PinchGestureHandlerGestureEvent, PinchGestureHandlerStateChangeEvent } from "react-native-gesture-handler";
+import Animated, { Value, add, event, cond, eq, set, interpolate, block, useCode } from "react-native-reanimated";
+
+const App = () => {
+  const scaleXY = useRef(new Value(1)).current;
+  const offset = useRef(new Value(0)).current;
+
+  const _onPinchGestureEvent = event<PinchGestureHandlerGestureEvent & PinchGestureHandlerStateChangeEvent>([{
+    nativeEvent: ({ scale, state, oldState }) => block([
+      cond(eq(state, State.ACTIVE), set(scaleXY, add(offset, scale))),
+      cond(eq(oldState, State.ACTIVE), set(offset, scale))
+    ])
+  }]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PinchGestureHandler
+      onGestureEvent={_onPinchGestureEvent}
+      onHandlerStateChange={_onPinchGestureEvent}
+      >
+      <Animated.View style={styles.container} collapsable={false}>
+        <Animated.Image
+          resizeMode="stretch"
+          source={require("./avatar.png")}
+          style={[
+            styles.pinchableImage,
+            {
+              transform: [
+                { perspective: 1000 },
+                { scale: scaleXY },
+              ],
+            },
+          ]}
+        />
+      </Animated.View>
+    </PinchGestureHandler>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
-});
+  pinchableImage: {
+    width: 182,
+    height: 180
+  }
+
+})
+
+export default App;
