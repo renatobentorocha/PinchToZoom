5594def12d8116b4c9af4fb279408f9530b71569
Renato Rocha <renatob.rocha@gmail.com>
2020-07-06 00:48:23 -0300

    Move and Pintch to Zoom

--- App.tsx
+++ App.tsx
@@ -2,42 +2,86 @@ import 'react-native-gesture-handler';
 
 import React, { useRef } from 'react';
 
-import { StyleSheet } from 'react-native';
-import { State, PinchGestureHandler, PinchGestureHandlerGestureEvent, PinchGestureHandlerStateChangeEvent } from "react-native-gesture-handler";
-import Animated, { Value, add, event, cond, eq, set, interpolate, block, useCode } from "react-native-reanimated";
+import { StyleSheet, View } from 'react-native';
+import { 
+  State, 
+  PanGestureHandler, 
+  PanGestureHandlerGestureEvent,
+  PanGestureHandlerStateChangeEvent,
+  PinchGestureHandler, 
+  PinchGestureHandlerGestureEvent, 
+  PinchGestureHandlerStateChangeEvent 
+} from "react-native-gesture-handler";
+
+import Animated, { Value, add, event, cond, eq, set, block, debug, and, multiply } from "react-native-reanimated";
 
 const App = () => {
+  const transX = useRef(new Value(0)).current;
+  const offsetX = useRef(new Value(0)).current;
+  
+  const transY = useRef(new Value(0)).current;
+  const offsetY = useRef(new Value(0)).current;
+
   const scaleXY = useRef(new Value(1)).current;
-  const offset = useRef(new Value(0)).current;
+  const offset = useRef(new Value(1)).current;
+
+  const _scale = multiply(offset, scaleXY);
+
+  const _onPanchGestureEvent = event<PanGestureHandlerGestureEvent & PanGestureHandlerStateChangeEvent>([{
+    nativeEvent: ({translationX, translationY, state, oldState}) => block([
+      cond(eq(state, State.ACTIVE), [
+        set(transX, add(offsetX, translationX)),
+        set(transY, add(offsetY, translationY)),
+      ]),
+      cond(eq(oldState, State.ACTIVE), [
+        set(offsetX, transX),
+        set(offsetY, transY),
+      ]),
+    ])
+  }]);
 
   const _onPinchGestureEvent = event<PinchGestureHandlerGestureEvent & PinchGestureHandlerStateChangeEvent>([{
     nativeEvent: ({ scale, state, oldState }) => block([
-      cond(eq(state, State.ACTIVE), set(scaleXY, add(offset, scale))),
-      cond(eq(oldState, State.ACTIVE), set(offset, scale))
+      cond(eq(state, State.ACTIVE), set(scaleXY, scale)),
+      cond(eq(oldState, State.ACTIVE), set(offset, _scale))
     ])
   }]);
 
   return (
-    <PinchGestureHandler
-      onGestureEvent={_onPinchGestureEvent}
-      onHandlerStateChange={_onPinchGestureEvent}
-      >
-      <Animated.View style={styles.container} collapsable={false}>
-        <Animated.Image
-          resizeMode="stretch"
-          source={require("./avatar.png")}
-          style={[
-            styles.pinchableImage,
-            {
-              transform: [
-                { perspective: 1000 },
-                { scale: scaleXY },
-              ],
-            },
-          ]}
-        />
+    <View style={styles.container}>
+    <PanGestureHandler 
+      onGestureEvent={_onPanchGestureEvent} 
+      onHandlerStateChange={_onPanchGestureEvent}>
+      <Animated.View 
+        style={[
+          {backgroundColor: "green"}, 
+          {
+            transform: [
+              {translateX: transX},
+              {translateY: transY},
+            ]
+          }]}>
+        <PinchGestureHandler
+          onGestureEvent={_onPinchGestureEvent}
+          onHandlerStateChange={_onPinchGestureEvent}>
+            <Animated.Image
+              resizeMode="stretch"
+              source={require("./avatar.png")}
+              style={[
+                styles.pinchableImage,
+                {
+                  transform: [
+                    { perspective: 1000 },
+                    
+                    { scale: _scale },
+                  ],
+                },
+              ]}
+            />
+        </PinchGestureHandler>
       </Animated.View>
-    </PinchGestureHandler>
+    </PanGestureHandler>
+    </View>
   );
 }
 
@@ -49,7 +93,8 @@ const styles = StyleSheet.create({
   },
   pinchableImage: {
     width: 182,
-    height: 180
+    height: 180,
+    backgroundColor: "#ddd",
   }
 
 })
